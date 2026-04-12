8409850410a746cff31c26204e1b57fbcf54e2e1
Jeremy Bouloux <git@jeremybouloux.com>
2026-04-12 09:40:00 +0200

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,15 @@
 return {
-  {
-    'catppuccin/nvim',
-    opts = {
-      flavour = 'macchiato',
-    },
-  },
-  -- Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-
+	{
+		"catppuccin/nvim",
+		opts = {
+			flavour = "macchiato",
+		},
+	},
+	-- Configure LazyVim to load catppuccin
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-nvim",
+		},
+	},
 }
